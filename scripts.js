05784f64c6450cb0c77b5971ddb695790be614ae
Gaven Tran <gaven.q.tran@gmail.com>
2023-05-22 18:54:17 -0400

    Create 16x16 grid using defined height and width
    I think it will be best to figure out how to use flex: 1; but this is
    what I made so far!

--- scripts.js
+++ scripts.js
@@ -1,11 +1,9 @@
-/* 
-*How do I make a 16x16 grid with DOM methods and CSS styling?
-*/
-
 const gridContainer = document.querySelector(".gridContainer");
 
-for(let i=1; i <= 5; i++) {
+for(let i=1; i <= (16*16); i++) {
     const gridItem = document.createElement("div");
     gridItem.classList.add("gridItems");
     gridContainer.appendChild(gridItem);
 }
+/* This loop works by creating a new node every iteration. Without including the createElement and classList.add declaration in the loop, the appendChild declaration will just repeatedly replace the node created from the last iteration. */
+/* Later, look to take the condition's equation (16*16) and apply the 16 to a variable so the user can use make a grid with however many items they want. */
