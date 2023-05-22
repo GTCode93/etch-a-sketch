5faafbde685e6b56824d6d1a89419b28e580db28
Gaven Tran <gaven.q.tran@gmail.com>
2023-05-22 19:27:06 -0400

    Attempt creating hover event listener

--- scripts.js
+++ scripts.js
@@ -7,3 +7,8 @@ for(let i=1; i <= (16*16); i++) {
 }
 /* This loop works by creating a new node every iteration. Without including the createElement and classList.add declaration in the loop, the appendChild declaration will just repeatedly replace the node created from the last iteration. */
 /* Later, look to take the condition's equation (16*16) and apply the 16 to a variable so the user can use make a grid with however many items they want. */
+
+gridItem.addEventListener("mouseover", (gridItem) => {
+    const gridItemsHovered = document.querySelectorAll(".gridItem");
+    gridItemsHovered.style.backgroundColor = "black";
+});
