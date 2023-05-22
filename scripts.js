25545c22a0c64da39eab5431737f8b7c1ae685f4
Gaven Tran <gaven.q.tran@gmail.com>
2023-05-22 18:32:52 -0400

    Style items & learn how to create multiple-same items

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
 /* 
 *How do I make a 16x16 grid with DOM methods and CSS styling?
 */
+
+const gridContainer = document.querySelector(".gridContainer");
+
+for(let i=1; i <= 5; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("gridItems");
+    gridContainer.appendChild(gridItem);
+}
