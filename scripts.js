a1b902f5651e6bad1fc4d1079e5459ac3cd8cc2b
Gaven Tran <gaven.q.tran@gmail.com>
2023-05-23 15:33:03 -0400

    Solve hover event listener issue
    I asked for help and Damon from TOP's Discord gave me some advice and
    guided me a bit!
    They pointed out that the inner gridItemsHovered node list cannot have
    the property classList to it since it's many items. What I needed to do
    was iterate through it the same way I had before with [i]:
    gridItemsHovered[i]!
    
    There's also a comment towards the bottom of the page on how to
    optimize the space and workflow of my code so far. I want to try their
    method later on after I figure out the FlexBox situation!

--- scripts.js
+++ scripts.js
@@ -9,14 +9,31 @@ for(let i=1; i <= (16*16); i++) {
 
 /* Later, look to take the condition's equation (16*16) and apply the 16 to a variable so the user can use make a grid with however many items they want. I will first need to figure out the grid styling first with FlexBox*/
 
-/* const gridItemsHover = document.querySelectorAll(".gridItems");
+const gridItemsHovered = document.querySelectorAll(".gridItems");
 
-gridItemsHover.addEventListener("mouseover", () => {
+for(let i=0; i < gridItemsHovered.length; i++) {
+    gridItemsHovered[i].addEventListener("mouseover", function() {
+        gridItemsHovered[i].classList.toggle("gridItemsHovered");
+    });
+}
 
+
+
+/* 
+ADVICE FROM TOP DISCORD:
+I'll write out how my current workflow compared to what Damon said to try.
+    My workflow:
+    Select the grid container, append the 256 items into it, select all the grid items into a node list, add event listener then
+
+    Damon's cleaner workflow suggestion:
+    Select the grid container, give the item's an event listener AS you append them, ends up much cleaner and with less code. The event listener and items themselves are created at the exact same time.
+*/
+
+
+
+
+
+/*This code was an alternative method using Array methods to iterate through the node list, but I did not use this version due to concerns that using Array methods on node lists is not best practice. But, it still does work!*/
+/* gridItemsHovered.forEach(box => {
+    box.addEventListener("mouseover", () => box.classList.add("gridItemsHovered"))
 }); */
-
-const headerElement = document.querySelector("#header");
-
-headerElement.addEventListener("mouseover", () => {
-    headerElement.classList.add("headerElement");
-});
