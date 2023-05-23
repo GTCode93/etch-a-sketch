676a3e57d128ccb3380ce1dcd45cce452bac8b9d
Gaven Tran <gaven.q.tran@gmail.com>
2023-05-23 14:23:30 -0400

    Redo hover EL demo, apply hover to h1 for practice

--- scripts.js
+++ scripts.js
@@ -6,9 +6,17 @@ for(let i=1; i <= (16*16); i++) {
     gridContainer.appendChild(gridItem);
 }
 /* This loop works by creating a new node every iteration. Without including the createElement and classList.add declaration in the loop, the appendChild declaration will just repeatedly replace the node created from the last iteration. */
-/* Later, look to take the condition's equation (16*16) and apply the 16 to a variable so the user can use make a grid with however many items they want. */
 
-gridItem.addEventListener("mouseover", (gridItem) => {
-    const gridItemsHovered = document.querySelectorAll(".gridItem");
-    gridItemsHovered.style.backgroundColor = "black";
+/* Later, look to take the condition's equation (16*16) and apply the 16 to a variable so the user can use make a grid with however many items they want. I will first need to figure out the grid styling first with FlexBox*/
+
+/* const gridItemsHover = document.querySelectorAll(".gridItems");
+
+gridItemsHover.addEventListener("mouseover", () => {
+
+}); */
+
+const headerElement = document.querySelector("#header");
+
+headerElement.addEventListener("mouseover", () => {
+    headerElement.classList.add("headerElement");
 });
