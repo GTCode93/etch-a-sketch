1f6faf441b7eaf63ea543346a4488db2e8ac8a32
Gaven Tran <gaven.q.tran@gmail.com>
2023-05-23 23:24:05 -0400

    Edit HTML and apply event listener to functions

--- scripts.js
+++ scripts.js
@@ -1,14 +1,19 @@
 const gridContainer = document.querySelector(".gridContainer");
-let userInputNumber; /* This variable is the W and H of the grid */
+let userInputNumber;
 
 function createGrid(userInputNumber) {
+
     for(let i=1; i <= (userInputNumber); i++) {
+        
         const gridRow = document.createElement("div");
         gridRow.classList.add("gridRows");
         gridContainer.appendChild(gridRow);
         for(let i=1; i <= (userInputNumber); i++) {
             const gridItem = document.createElement("div");
             gridItem.classList.add("gridItems");
+            gridItem.addEventListener("mouseover", () => {
+                gridItem.classList.add("gridItemHovered")
+            })
             gridRow.appendChild(gridItem);
         }
     }
@@ -21,20 +26,11 @@ for(let i=1; i <= (16); i++) {
     for(let i=1; i <= (16); i++) {
         const gridItem = document.createElement("div");
         gridItem.classList.add("gridItems");
+        gridItem.addEventListener("mouseover", () => {
+            gridItem.classList.add("gridItemHovered")
+        })
         gridRow.appendChild(gridItem);
     }
-}
-
-/* This loop works by creating a new node every iteration. Without including the createElement and classList.add declaration in the loop, the appendChild declaration will just repeatedly replace the node created from the last iteration. */
-
-/* Later, look to take the condition's equation (16*16) and apply the 16 to a variable so the user can use make a grid with however many items they want. I will first need to figure out the grid styling first with FlexBox*/
-
-const gridItemsHovered = document.querySelectorAll(".gridItems");
-
-for(let i=0; i < gridItemsHovered.length; i++) {
-    gridItemsHovered[i].addEventListener("mouseover", function() {
-        gridItemsHovered[i].classList.add("gridItemsHovered");
-    });
 }
 
 const gridButton = document.querySelector(".gridButton");
@@ -43,17 +39,13 @@ gridButton.addEventListener("click", () => {
     createGrid(userInputNumber);
 });
 
-
-
-
-
-
-
-
-
-
-
-
+/* const gridItemsHovered = document.querySelectorAll(".gridItems");
+for(let i=0; i < gridItemsHovered.length; i++) {
+    gridItemsHovered[i].addEventListener("mouseover", function() {
+        gridItemsHovered[i].classList.add("gridItemsHovered");
+    });
+}
+*/
 
 /* 
 ADVICE FROM TOP DISCORD:
@@ -64,10 +56,6 @@ I'll write out how my current workflow compared to what Damon said to try.
     Damon's cleaner workflow suggestion:
     Select the grid container, give the item's an event listener AS you append them, ends up much cleaner and with less code. The event listener and items themselves are created at the exact same time.
 */
-
-
-
-
 
 /*This code was an alternative method using Array methods to iterate through the node list, but I did not use this version due to concerns that using Array methods on node lists is not best practice. But, it still does work!*/
 /* gridItemsHovered.forEach(box => {
